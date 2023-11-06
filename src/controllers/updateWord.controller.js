a2dfb258094430589ac7f7531cd6c2e259f768d3
Jose  Gutierrez <85263213+josedugu@users.noreply.github.com>
2023-11-06 18:45:30 -0500

    updated cors

--- src/controllers/updateWord.controller.js
+++ src/controllers/updateWord.controller.js
@@ -2,20 +2,26 @@ const Words = require("../models/words.model");
 
 const updateWord = async (req, reply) => {
   reply.header("Access-Control-Allow-Origin", "*");
-  reply.header("Access-Control-Allow-Methods", "POST");
-  const { studentKey, addWords } = req.body;
-  console.log(req.body);
+  reply.header("Access-Control-Allow-Methods", "POST, OPTIONS");
+  const body=JSON.parse(req.body);
+  console.log("body",body);
+  const { studentKey, addWords } = body;
+  //console.log(req.body);
   try {
-    const studentWords=await Words.findOneAndUpdate(
-      { studentKey: studentKey }, 
-      { $push: { words: { $each: addWords } } }, 
-      { returnNewDocument: true } 
+    const studentWords = await Words.findOneAndUpdate(
+      { studentKey: studentKey },
+      { $push: { words: { $each: addWords } } },
+      { returnNewDocument: true }
     );
-    console.log(studentWords);
+    //console.log("studentWords",studentWords);
+    reply.header("Access-Control-Allow-Origin", "*");
+    reply.header("Access-Control-Allow-Methods", "POST, OPTIONS");
     reply.code(200);
     reply.send("SUCCESS");
     console.log("PETICION COMPLETADA");
   } catch (error) {
+    reply.header("Access-Control-Allow-Origin", "*");
+    reply.header("Access-Control-Allow-Methods", "POST, OPTIONS");
     console.log(error);
     reply.code(400);
   }
